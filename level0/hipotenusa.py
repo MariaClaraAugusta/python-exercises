a207ed7f2a667e533bcff755070d835b0660df90
Maria Clara <136365112+MariaClaraAugusta@users.noreply.github.com>
2024-02-23 20:44:17 -0300

    Create hipotenusa.py

---
+++ level0/hipotenusa.py
@@ -0,0 +1,8 @@
+#calculo da hipotenusa
+
+cateto1 = float(input("Cateto 1? "))
+cateto2 = float(input("Cateto 2? "))
+
+hipotenusa = (cateto1 ** 2 + cateto2 ** 2) ** 0.5
+
+print("Hipotenusa:", hipotenusa)
